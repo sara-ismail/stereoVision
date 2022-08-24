8e9b2466d6998267541d698da6b76f41c8aa1bcc
Sara Ismail <76042788+sara-ismail@users.noreply.github.com>
2022-08-24 15:27:26 +0100

    calculates and displays FPS on both stream videos

---
+++ fps_calc.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2
+import time
+import camera_stream
+ 
+
+cap = camera_stream.CameraVideoStream()
+cap2 = camera_stream.CameraVideoStream()
+
+cv2.namedWindow("Stream 1", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Stream 2", cv2.WINDOW_NORMAL)
+
+prev_frame_time = 0
+new_frame_time = 0
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+while(cap.isOpened() and cap2.isOpened()):
+ 
+    ret, frame = cap.read()
+    ret2, frame2 = cap2.read()
+
+    if not (ret or ret2):
+        break
+ 
+    new_frame_time = time.time()
+
+    fps = 1/(new_frame_time-prev_frame_time)
+    prev_frame_time = new_frame_time
+ 
+    fps = int(fps)
+    fps = str(fps)
+
+    cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+    cv2.putText(frame2, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+
+    cv2.imshow('Stream 1', frame)
+    cv2.imshow('Stream 2', frame2)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cap2.release()
+
+cv2.destroyAllWindows()
