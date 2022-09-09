5dccbe6232c415a87dc12d1a24ab2b521ccef618
durhamadl <Toby Breckon>
2022-09-09 13:48:16 +0100

    Updated fps_calc.py

--- fps_calc.py
+++ fps_calc.py
@@ -4,8 +4,8 @@ import time
 import camera_stream
  
 
-cap = camera_stream.CameraVideoStream()
-cap2 = camera_stream.CameraVideoStream()
+cap = camera_stream.CameraVideoStream("thetauvcsrc ! decodebin ! autovideoconvert ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink", cv2.CAP_GSTREAMER)
+cap2 = camera_stream.CameraVideoStream("thetauvcsrc ! decodebin ! autovideoconvert ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink", cv2.CAP_GSTREAMER)
 
 cv2.namedWindow("Stream 1", cv2.WINDOW_NORMAL)
 cv2.namedWindow("Stream 2", cv2.WINDOW_NORMAL)
@@ -37,7 +37,7 @@ while(cap.isOpened() and cap2.isOpened()):
     cv2.imshow('Stream 1', frame)
     cv2.imshow('Stream 2', frame2)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('x'):
         break
 
 cap.release()
